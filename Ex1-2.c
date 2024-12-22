4bcc85beb688edf66ef97e6eb21c77868e4a38a3
Pouria Dana <pouriadana73@gmail.com>
2024-12-22 10:49:24 +0330

    Create Exercise 1-2

---
+++ Ex1-2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    // printf("Hello, world!\e");                 // creates a command pipeline
+    // printf("Hello, world!\r");                 // does not print line
+    // printf("Hello, world!\u");                 // universal character name
+    // printf("Hello, world!\vHello\vHello");     // A newline alternative?
+    // printf("Hello, world!\x");                 // Used for hexidecimal digits
+    prinf("Hello, World!\n");
+}
