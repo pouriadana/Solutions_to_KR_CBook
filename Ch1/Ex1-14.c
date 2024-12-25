68b828bf4cc80d68af5b06f92c9af019988aed5c
Pouria Dana <pouriadana73@gmail.com>
2024-12-25 08:27:09 +0330

    (ver1.1) support digits

--- Ch1/Ex1-14.c
+++ Ch1/Ex1-14.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
+#define DIGIT 10
 #define ALEPH 26
 
 int main()
 {
     int c;
     int faleph[ALEPH];
+    int fdigit[DIGIT];
 
     for (int i = 0; i < ALEPH; ++i) {
         faleph[i] = 0;
+    }
+    for (int i = 0; i < DIGIT; ++i) {
+        fdigit[i] = 0;
     }
 
     while ((c = getchar()) != EOF) {
@@ -18,11 +23,22 @@ int main()
         else if ('a' <= c && 'z' >= c) {
             ++faleph[c - 'a'];
         }
+        else if ('0' <= c && '9' >= c) {
+            ++fdigit[c - '0'];
+        }
     }
 
     for (int i = 0; i < ALEPH; ++i) {
         printf("%c\t", 'A' + i);
         for (int inner_i = 0; inner_i < faleph[i]; ++inner_i) {
+            putchar('#');
+            putchar(' ');
+        }
+        putchar('\n');
+    }
+    for (int i = 0; i < DIGIT; ++i) {
+        printf("%c\t", '0' + i);
+        for (int inner_i = 0; inner_i < fdigit[i]; ++inner_i) {
             putchar('#');
             putchar(' ');
         }
