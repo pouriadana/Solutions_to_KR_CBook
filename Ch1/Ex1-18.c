83a6cd9bd17cb2b27452d4ced0ece0b8ebf4ca75
Pouria Dana <pouriadana73@gmail.com>
2024-12-27 17:44:32 +0330

    Create Exercise 1-18

---
+++ Ch1/Ex1-18.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+
+#define MAXLINE 1000     // set to 100 for demonstration purposes
+
+int getmyline(char line[], int maxline);
+void processline(char line[], int lim);
+int calulatelength(char line[]);
+
+int main()
+{
+    int len, max;
+    char line[MAXLINE];
+
+    max = len = 0;
+    while ((len = getmyline(line, MAXLINE)) > 0) {
+        if (len > MAXLINE) {
+            line[MAXLINE - 2] = '\n';
+        }
+        printf("%d\n", len);
+        printf("Line's length before processing:\t%d\n", calulatelength(line));
+        processline(line, len);
+        printf("Line's length after processing:\t%d\n", calulatelength(line));
+        // printf("%s",line);
+    }
+}
+
+int getmyline(char line[], int lim)
+{
+    int i, c, extra;
+    extra = 0;
+
+    for (i = 0; (c = getchar()) != EOF && c != '\n' && i < lim-1; ++i) {
+        line[i] = c;
+    }
+    if (c == '\n') {
+        line[i] = c;
+        ++i;
+    }
+    line[i] = '\0';
+    if (c != '\n' && c != EOF) {
+        ++extra;
+        while ((c = getchar()) != '\n' && c != EOF) {
+            ++extra;
+        }
+    }
+    return i + extra;
+}
+
+void processline(char line[], int len)
+{
+    // printf("line[len - 1]: %d\n", line[len - 1]);
+    if (len > 1) {
+        while (len > 1 && (line[len - 2] == ' ' || line[len - 2] == '\t')) {
+            // printf("Inside while statement\n");
+            if (line[len - 2] == ' ' || line[len - 2] == '\t') {
+                line[len - 2] = '\n';
+                line[len - 1] = '\0';
+            }
+            // printf("%d\n", len);
+            --len;
+        }
+    }
+}
+
+int calulatelength(char line[])
+{
+    int i;
+
+    for (i = 0; line[i] != '\0'; ++i)
+        ;
+    return i;
+}
