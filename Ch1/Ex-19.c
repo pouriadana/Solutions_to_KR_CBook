024f3840ecc288395488ef00e21116369a316736
Pouria Dana <pouriadana73@gmail.com>
2024-12-27 22:52:07 +0330

    Add function to reverse
    Function returns an integer to ascertain successful completion. Adds null character at the end of the reversed character string.

--- Ch1/Ex-19.c
+++ Ch1/Ex-19.c
@@ -3,15 +3,19 @@
 #define MAXLINE 1000
 
 int getmyline(char input[], int lim);
+int revline(char toreverse[], char originalline[], int lim);
 
 int main()
 {
     char line[MAXLINE];
+    char rline[MAXLINE];
     int n;
 
     while ((n = getmyline(line, MAXLINE)) > 1) {
-        printf("%s\n", line);
+        printf("original:\t%s\n", line);
         // reverse character here
+        revline(rline, line, n);
+        printf("Reversed:\t%s\n", rline);
     }
 }
 
@@ -23,5 +27,18 @@ int getmyline(char input[], int lim)
         input[i] = c;
     }
     input[i] = '\0';
+    return i;
+}
+
+int revline(char toreverse[], char originalline[], int lim)
+{
+    int i, j;
+
+    j = 0;
+    for (i = lim - 1; i >= 0; --i, ++j) {
+        //printf("inside loop\n");
+        toreverse[j] = originalline[i];
+    }
+    toreverse[j] = '\0';
     return i;
 }
