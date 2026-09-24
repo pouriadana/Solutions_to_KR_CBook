de49f35128acd014face1e61aece13c9053917df
Pouria Dana <pouriadana73@gmail.com>
2026-09-24 08:16:21 +0000

    Change name to reflect program pos. in textbook

---
+++ Chapter 1/arrays1.6.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/* Arrays 1.6 | First in-text program */
+/* count digits via an array, white space and others in variables */
+int main()
+{
+    int c, i, nwhite, nother;
+    int ndigit[10];
+
+    nwhite = nother = 0;
+    for (i = 0; i < 10; ++i)
+        ndigit[i] = 0;
+
+    while ((c = getchar()) != EOF) {
+        if ('0' <= c && c <= '9')
+            ++ndigit[c-'0'];
+        else if (c == ' ' || c == '\n' || c == '\t')
+            ++nwhite;
+        else
+            ++nother;
+    }
+
+    printf("digits =");
+    for (i = 0; i < 10; ++i)
+        printf(" %d", ndigit[i]);
+    printf(", white space = %d, other = %d\n", nwhite, nother);
+}
